eeeda5590f2990a0205164860026cbfeb0dc2186
Pedro Imbriani <pedro.imbriani.clear@gmail.com>
2023-03-27 12:34:14 -0300

    useContext

--- hooks/src/index.js
+++ hooks/src/index.js
@@ -4,7 +4,11 @@ import React from 'react'
 
 import App from './views/App'
 
+import { DataContext,data } from './data/DataContext'
+
 ReactDOM.render(
+  
     <App />,
+   
     document.getElementById('root')
 )
